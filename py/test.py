562b81ad0d425db0a1bc3e8a3d5637b6b765d43e
DESKTOP-7B10SV1\Aquib <bsse0718@iit.du.ac.bd>
2018-10-02 17:51:59 +0600

    templates added

--- py/test.py
+++ py/test.py
@@ -1,11 +1,22 @@
 from werkzeug.wrappers import Request, Response
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    rendered = render_template('home.html')
+    return rendered
+
+
+@app.route("/result", methods=['POST'])
+def result():
+
+    searched_word = request.form['search_key']
+    rendered = render_template('result.html', key=searched_word)
+    return searched_word
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
