bb621d38f1b82ea75f78a20923ca2bd7ffe242b7
Aquib Azmain <bsse0718@iit.du.ac.bd>
2018-10-03 01:36:54 +0600

    parser updatte

--- py/htmlParser.py
+++ py/htmlParser.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get('https://api.github.com/events')
+r = requests.get('https://api.github.com/')
 print(r.url)
 html_doc = r.text
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-print(soup.prettify())
+#print(soup.prettify())
+for link in soup.find_all('a'):
+    print(link.get('href'))
