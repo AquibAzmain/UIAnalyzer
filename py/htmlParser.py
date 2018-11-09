f009c56b448f3d5eff62e28e2a678868770d1209
Aquib Azmain <bsse0718@iit.du.ac.bd>
2018-11-10 01:55:06 +0600

    website cloner added

--- py/htmlParser.py
+++ py/htmlParser.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
+import json
 import requests
 
-r = requests.get('https://api.github.com/')
+r = requests.get('http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168')
 print(r.url)
 html_doc = r.text
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-#print(soup.prettify())
+# print(soup.prettify())
 for link in soup.find_all('a'):
     print(link.get('href'))
+
+soup.find_all('a')    
